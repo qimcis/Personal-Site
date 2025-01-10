efb36f6753cac797dc12deafbcbdad1a674c3494
qimcis <chixie.mcisaac@gmail.com>
2025-01-10 00:44:02 -0500

    please work...

--- site/app/page.js
+++ site/app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import MailTo from "./components/mailto";
-import { Github, Linkedin } from "lucide-react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import Link from "next/link";
 
 export default function Home() {
@@ -16,10 +16,10 @@ export default function Home() {
           <div className="mt-5 flex items-center gap-4">
             <MailTo label="contact me" mailto="chixie.mcisaac@gmail.com" className="bg-black text-2xl text-white p-2 rounded-md w-fit"/>
             <Link href="https://github.com/qimcis" target="_blank" rel="noopener noreferrer">
-              <Github size={32} strokeWidth={1.1} className="text-black hover:text-gray-500 transition-colors" />
+              <FaGithub size={32} className="text-black hover:text-gray-500 transition-colors" />
             </Link>
             <Link href="https://linkedin.com/in/chimcisaac" target="_blank" rel="noopener noreferrer">
-              <Linkedin size={32} strokeWidth={1.1} className="text-black hover:text-gray-500 transition-colors" />
+              <FaLinkedin size={32} className="text-black hover:text-gray-500 transition-colors" />
             </Link>
           </div>
         </div>
