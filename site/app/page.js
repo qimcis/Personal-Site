bfc62207e17d7331d90dc23d9f7be1cf24e5ad28
chimcis <chi@chromaticdata.com>
2025-01-29 15:42:41 -0500

    status update

--- site/app/page.js
+++ site/app/page.js
@@ -6,11 +6,12 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center -mt-5 m-10">
-      <Image src='/clarissa_cat_transparent.png' width={512} height={512} alt="cat"/>
+      <Image src='/clarissa_cat_transparent.png' width={450} height={450} alt="cat"/>
       <div className="flex flex-col">
         <div className="items-left justify-left">
           <h1 className="text-black mt-5 text-8xl font-bold">chi mcisaac</h1>
           <p className="text-black mt-5 text-2xl">1st year cs student at Carleton University</p>
+          <div className="text-black mt-5 text-2xl">Looking for SWE internship opportunities <p className="break-words">for Summer/Fall 2025</p></div>
           <p className="text-black mt-5 text-2xl">swe intern @ <a href="https://www.chromaticdata.com/" className="underline hover:text-gray-600" target="_blank">Chromatic Data</a></p>
           <p className="text-black mt-5 text-2xl">swe intern @ <a href="https://hivenue.ca/en" className="underline hover:text-gray-600" target="_blank">Hivenue</a></p>
           <div className="mt-5 flex items-center gap-4">
